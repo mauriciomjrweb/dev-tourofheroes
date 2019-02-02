722928f64162cd30a94f0fca6edb3b5db49451b6
mauriciojrweb <mauriciojr.web@hotmail.com>
2019-02-02 15:12:44 -0200

    Aplicação sem HttpClient

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,13 +12,29 @@ export class HeroesComponent implements OnInit {
 
   //hero = 'Windstorm';
 
-  heroes = HEROES;
+  heroes: Hero[];
+  selectedHero: Hero;
 
-  hero: Hero = { id: 1, name: 'Windstorm' };
-
-  constructor() { }
+  constructor( private heroService: HeroService ) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
+
+  /* Utilizando a chamada sincrona
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  */
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe( heroes => this.heroes = heroes );
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 
 }
